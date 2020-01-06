99c65cd6bc6dd64fa391a7ed8fb8bba8095fe829
Shubham Kumar Jha <skjha60070@gmail.com>
2020-01-07 00:47:27 +0530

    Added Files for Robofriend

---
+++ precache-manifest.16e3fe87c28b4a55998a6c57f8b92832.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca4e26175ad6751e587cfbea2d1229f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72e1f10f560fc6235d38",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "23faa1927adc103204a5",
+    "url": "/static/css/main.ca3b69e2.chunk.css"
+  },
+  {
+    "revision": "72e1f10f560fc6235d38",
+    "url": "/static/js/2.d81826f8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d81826f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23faa1927adc103204a5",
+    "url": "/static/js/main.e86dd86b.chunk.js"
+  },
+  {
+    "revision": "d433ce91ffe4c307d52c",
+    "url": "/static/js/runtime-main.ef475df4.js"
+  }
+]);
